84c5fb2882093567549fdd24be8fbd70db9bc808
5alikorkmaz <korkmaz65ali_@hotmail.com>
2025-04-21 10:08:15 +0300

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import PizzaForm from './pages/PizzaForm';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PizzaForm from "./pages/PizzaForm";
+import OrderSuccess from "./pages/OrderSuccess";
 
 function App() {
   return (
@@ -9,10 +9,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pizza" element={<PizzaForm />} />
+        <Route path="/success" element={<OrderSuccess />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
